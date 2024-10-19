dfcde69c5544808f8c3f7e69037ded59918d887a
bell-nat <bell-nat@yandex.ru>
2024-10-19 16:20:43 +0300

    completed

--- main.nim
+++ main.nim
@@ -1,8 +1,43 @@
-type
-    # Для каждого объекта и атрибута после названия добавьте звездочку.
-    # Код для приюта (Shelter)
+type    
+    Manager* = object
+        name*: string
+        post*: string
+    Staff* = object
+        name*: string
+        birthDate*: string
+        uid*: int
+    Pet* = object
+        name*: string
+        age*: int
     Shelter* = object
-
-    # Код для школы (School)
-
-    # Код для магазина (Shop)
+        manager*: Manager
+        staff*: Staff
+        pet*: Pet
+    Director* = object
+        name*: string
+    Teacher* = object
+        name*: string
+        class*: string
+    Student* = object
+        name*: string
+        class*: string
+    School* = object
+        director*: Director
+        teacher*: Teacher
+        student*: Student
+    ShopManager* = object
+        name*: string
+    ShopStaff* = object
+        name*: string
+        birthDate*: string
+        post*: string
+    Good* = object
+        title*: string
+        endDate*: string
+    Cash* = object
+        number*: int
+    Shop* = object
+        manager*: ShopManager
+        staff*: ShopStaff
+        good*: Good
+        cash*: Cash
